f56c422456f503dfd913f09da8308ce1f188040b
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-09-07 22:45:02 +0545

    images

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import "./global.css";
 export default function index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+      <Text className="text-xl font-bold text-blue-600">
         Welcome to Nativewind!
       </Text>
     </View>
