05383aeba53af89a378ca6d07d9eda0b7e60351a
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-09-15 23:58:10 +0545

    Auth layout complete

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,7 +1,7 @@
 import { images } from '@/constants'
 import { Slot } from 'expo-router'
 import React from 'react'
-import { Dimensions, ImageBackground, KeyboardAvoidingView, Platform, ScrollView, Text, View } from 'react-native'
+import { Dimensions, Image, ImageBackground, KeyboardAvoidingView, Platform, ScrollView, Text, View } from 'react-native'
 
 export default function _layout() {
   return (
@@ -10,7 +10,7 @@ export default function _layout() {
       <ScrollView className='bg-white h-full' keyboardShouldPersistTaps='handled'>
         <View className='w-full relative' style={{height:Dimensions.get('screen').height/2.25}}>
           <ImageBackground source={images.loginGraphic} className='size-full rounded-b-lg' resizeMode='stretch'/>
-       
+          <Image source={images.logo} className='self-center size-48 absolute -bottom-16 z-10'/>
         </View>
       </ScrollView>
       <Text>Auth Layout</Text>
