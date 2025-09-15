5b5371aa6577c36138f0f101acd7b16c8753f1b4
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-09-15 23:56:41 +0545

    Auth screen design

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,10 +1,18 @@
+import { images } from '@/constants'
 import { Slot } from 'expo-router'
 import React from 'react'
-import { KeyboardAvoidingView, Text } from 'react-native'
+import { Dimensions, ImageBackground, KeyboardAvoidingView, Platform, ScrollView, Text, View } from 'react-native'
 
 export default function _layout() {
   return (
-    <KeyboardAvoidingView>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+
+      <ScrollView className='bg-white h-full' keyboardShouldPersistTaps='handled'>
+        <View className='w-full relative' style={{height:Dimensions.get('screen').height/2.25}}>
+          <ImageBackground source={images.loginGraphic} className='size-full rounded-b-lg' resizeMode='stretch'/>
+       
+        </View>
+      </ScrollView>
       <Text>Auth Layout</Text>
       <Slot/>
     </KeyboardAvoidingView>
