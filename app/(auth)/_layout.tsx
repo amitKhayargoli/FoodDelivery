295ed55712e24020a056a023066b6d52a77978f5
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-09-16 23:42:45 +0545

    auth layout with custom input

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,7 +1,9 @@
+import CustomButton from '@/components/CustomButton'
+import CustomInput from '@/components/CustomInput'
 import { images } from '@/constants'
 import { Slot } from 'expo-router'
 import React from 'react'
-import { Dimensions, Image, ImageBackground, KeyboardAvoidingView, Platform, ScrollView, Text, View } from 'react-native'
+import { Dimensions, Image, ImageBackground, KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native'
 
 export default function _layout() {
   return (
@@ -10,10 +12,19 @@ export default function _layout() {
       <ScrollView className='bg-white h-full' keyboardShouldPersistTaps='handled'>
         <View className='w-full relative' style={{height:Dimensions.get('screen').height/2.25}}>
           <ImageBackground source={images.loginGraphic} className='size-full rounded-b-lg' resizeMode='stretch'/>
-          <Image source={images.logo} className='self-center size-48 absolute -bottom-16 z-10'/>
+          <Image source={images.logo} className='self-center size-48 absolute -bottom-5 z-10'/>
         </View>
+
+      <CustomInput
+      placeholder='Enter your email'
+      value={''}
+      onChangeText={(text)=>{}}
+      label='Email'
+      keyboardType='email-address'
+      />
+      <CustomButton/>
+
       </ScrollView>
-      <Text>Auth Layout</Text>
       <Slot/>
     </KeyboardAvoidingView>
   )
