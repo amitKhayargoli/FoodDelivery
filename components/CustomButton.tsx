beff44de881c6fedda3146a7c92f9ec69cae9b7a
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-09-17 23:53:24 +0545

    Custom button with loading states

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,11 +1,30 @@
+import { CustomButtonProps } from '@/type'
+import cn from 'clsx'
 import React from 'react'
-import { Text, View } from 'react-native'
-
-const CustomButton = () => {
+import { ActivityIndicator, Text, TouchableOpacity, View } from 'react-native'
+const CustomButton = ({
+  onPress,
+  title="Click me",
+  style,
+  textStyle,
+  leftIcon,
+  isLoading = false,
+}: CustomButtonProps) => {
   return (
-    <View>
-      <Text>CustomButton</Text>
-    </View>
+      <TouchableOpacity className={cn('custom-btn',style)} onPress={onPress} >
+
+        {leftIcon}
+
+        <View className='flex-center flex-row'>
+          {
+            isLoading? (
+             <ActivityIndicator size="small" color="#fff" />
+            ) : (
+              <Text className={cn('text-white-100 paragraph-semibold',textStyle)}>{title}</Text> 
+            )
+          }
+        </View>
+    </TouchableOpacity>
   )
 }
 
