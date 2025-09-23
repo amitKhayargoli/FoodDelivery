15357270d51198551b56eb0f422657c30fac20a1
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-09-23 22:57:13 +0545

    tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,19 @@
-import { Redirect, Slot } from 'expo-router';
+import { images } from '@/constants';
+import { TabBarIconProps } from '@/type';
+import { Redirect, Tabs } from 'expo-router';
 import React from 'react';
+import { Image, Text, View } from 'react-native';
+
+const TabBarIcon = ({focused, icon, title}:TabBarIconProps) => {
+  return (
+    <View className='tab-icon'>
+      <Image source={icon} className='size-7' resizeMode='contain' tintColor={focused? '#FE8C00':'#5D5F6D'}></Image>
+      <Text>{title}</Text>
+   
+    </View>
+  )
+
+}
 
 export default function _layout() {
 
@@ -8,5 +22,9 @@ export default function _layout() {
   const isAuthenticated = false;
 
   if(!isAuthenticated) return <Redirect href="/signin" />
-  return <Slot />
+  return (
+    <Tabs>
+      <Tabs.Screen name="index" options={{ title: 'Home', tabBarIcon:({focused}) => <TabBarIcon title="Home" icon={images.home}  focused={focused} /> }} />
+    </Tabs>
+  )
 }
